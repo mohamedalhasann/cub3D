b344fac86f74567ea63cbed9b2c3e4a3041ce6dd
mohamedalhasann <alhsnmoha@gmail.com>
2026-07-25 16:14:03 +0300

    edit cub3D.h

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -20,11 +20,39 @@
 # define KEY_ESC 65307
 
 
+// parsing strucutres
+typedef struct s_color
+{
+    int r;
+    int g;
+    int b;
+}   t_color;
+typedef struct s_point
+{
+    int x;
+    int y;
+} t_point;
+typedef struct s_parse_data
+{
+    // these are all flags 1,0 if they exist
+    int north_flag;
+    int south_flag;
+    int west_flag;
+    int east_flag;
+    int floor_color_flag;
+    int ceiling_color_flag;
+    int player_count; // tracks N,S,E,W spawns (if its != 1 m the map hass missing or multiple player spawn points)
+}t_parse_data;
+
+
+// --------------------
+
 typedef struct s_img
 {
     void *image_ptr;
     int height;
     int width;
+    int bpp;
     char *address;
     int len;
     int endian;
@@ -32,13 +60,44 @@ typedef struct s_img
 
 typedef struct s_player
 {
-    double pos_x;
-    double y;
+    char in_map_spawn; // N,S,W,E from map
+    double pos_x; // position on grid
+    double pos_y; // same here
+    double dir_x; // vector x 
+    double dir_y; // vecotr y
+    double plane_x; // camera x
+    double plane_y; // camera y
+    double move_speed; // step distance per frame
+    double rot_speed; // turn angle per frame in radians
 }   t_player;
 
 typedef struct s_ray
 {
-    double camera;
+    int map_x; // current integer map square x the ray is in
+    int map_y; //  ---- y
+    double camera_x; // -1/0/1
+    double ray_x;
+    double ray_y;
+
+    double side_dist_x;
+    double side_dist_y;
+    double delta_dist_x;
+    double delta_dist_y;
+    int step_x;
+    int step_y;
+    int hit; // 1 if ray hit a wall, 0 if still moving
+    int side; // 1 if hit N/S wall , 0 if hit E/W wall
+    
+    double perp_wall_dist;
+    int line_height;
+    int draw_start;
+    int draw_end;
+    double wall_x;
+    int texture_x;
+    int texture_y;
+    double step;
+    double texture_position;
+
 }   t_ray;
 
 typedef struct s_map
@@ -48,6 +107,16 @@ typedef struct s_map
     int height;
     int floor_color;
     int ceiling_color;
+    char *north_path;
+    char *south_path;
+    char *west_path;
+    char *east_path;
+
+    t_img north_image;
+    t_img west_image;
+    t_img east_image;
+    t_img south_image;
+    
 }   t_map;
 
 typedef struct s_game
