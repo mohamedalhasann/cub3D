3ac238f10cd3b90d9a3b062c670fc10478a74d71
mohamedalhasann <alhsnmoha@gmail.com>
2026-08-12 15:35:41 +0300

    migrate shared cub3D header from minilibx to mlx42

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -1,23 +1,16 @@
 #ifndef CUB3D_H
 # define CUB3D_H
+
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+# include <math.h> 
 # include <fcntl.h>
 # include "../lib/libft/libft.h"
-# include "../minilibx-linux/mlx.h"
-# include "../lib/gnl/srcs/get_next_line.h"
-
+# include "../MLX42/include/MLX42/MLX42.h"
 
 # define SCREEN_WIDTH  1024
 # define SCREEN_HEIGHT 768
-# define KEY_W 119
-# define KEY_A 97
-# define KEY_S 115
-# define KEY_D 100
-# define KEY_LEFT 65361
-# define KEY_RIGHT 65363
-# define KEY_ESC 65307
 
 // parsing strucutres
 typedef struct s_color
@@ -26,13 +19,11 @@ typedef struct s_color
     int g;
     int b;
 }   t_color;
-
 typedef struct s_point
 {
     int x;
     int y;
 } t_point;
-
 typedef struct s_parse_data
 {
     // these are all flags 1,0 if they exist
@@ -42,26 +33,22 @@ typedef struct s_parse_data
     int east_flag;
     int floor_color_flag;
     int ceiling_color_flag;
+    int map_started; // set to 1 when map lines begin
     int player_count; // tracks N,S,E,W spawns (if its != 1 m the map hass missing or multiple player spawn points)
 }t_parse_data;
+
 
 // --------------------
 
 typedef struct s_img
 {
-    void *image_ptr;
-    int height;
-    int width;
-    int bpp;
-    char *address;
-    int len;
-    int endian;
+    xpm_t   *xpm;
 }   t_img;
 
 typedef struct s_player
 {
-    double x;
-    double y;
+    int x;
+    int y;
     char in_map_spawn; // N,S,W,E from map
     double pos_x; // position on grid
     double pos_y; // same here
@@ -75,7 +62,6 @@ typedef struct s_player
 
 typedef struct s_ray
 {
-    double camera;
     int map_x; // current integer map square x the ray is in
     int map_y; //  ---- y
     double camera_x; // -1/0/1
@@ -90,7 +76,7 @@ typedef struct s_ray
     int step_y;
     int hit; // 1 if ray hit a wall, 0 if still moving
     int side; // 1 if hit N/S wall , 0 if hit E/W wall
-
+    
     double perp_wall_dist;
     int line_height;
     int draw_start;
@@ -100,14 +86,11 @@ typedef struct s_ray
     int texture_y;
     double step;
     double texture_position;
+
 }   t_ray;
 
 typedef struct s_map
 {
-    char    *av;
-    char    **fullmap;
-    int     map_len;
-
     char **grid;
     int width;
     int height;
@@ -117,25 +100,38 @@ typedef struct s_map
     char *south_path;
     char *west_path;
     char *east_path;
-
-    int     file_len;
-    char    *file_name;
-    char    **file_content;
-
     t_img north_image;
     t_img west_image;
     t_img east_image;
     t_img south_image;
+
 }   t_map;
 
 typedef struct s_game
 {
-    void        *mlx;
-    void        *win;
-    t_img       frame;
+    mlx_t       *mlx;
+    mlx_image_t *frame;
     t_map       map;
     t_player    player;
 }   t_game;
 
-
+// --------------------
+void	init_mlx(t_game *game);
+void	close_game(t_game *game);
+void	hooks_handler(void *param);
+void    print_error_message(t_game *game,char *message);
+void	load_game_textures(t_game *game);
+void	my_mlx_pixel_put(mlx_image_t *img, int x, int y, unsigned int color);
+void	draw_floor_ceiling(t_game *game);
+void	render_game(void *param);
+int		is_wall(t_map *map, int x, int y);
+void	init_ray_direction(t_player *player, t_ray *ray ,int x);
+void	calculate_distance(t_player *p, t_ray *ray);
+void	dda_algorithm(t_ray *ray, t_map *map);
+unsigned int	get_texture_color(t_img *texture, int x, int y);
+void		get_wall_texture(t_game *game, t_ray *ray, t_img **texture);
+void	draw_wall_slice(t_game *game, t_ray *ray, int draw_start, int draw_end, int x);
+void	shoot_rays(t_game *game);
+int		parse_map_file(t_game *game, const char *path);
 #endif
+
